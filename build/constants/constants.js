9229f1027d49070a7aba632fff8097b97e6b6dc6
TheoSoko <theo.sokolowsky@gmail.com>
2023-10-25 17:22:56 +0200

    rm build after merge with prod

--- build/constants/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.visibility = void 0;
-exports.visibility = {
-    'private': 1,
-    'friends': 2,
-    'public': 3
-};
